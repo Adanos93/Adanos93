a4e4c0e71658a3387d2ac78e5912384eab2db15c
Adanos93 <adam.balazs.herczog@gmail.com>
2021-09-02 23:26:24 +0200

    gyakorlás

--- HTML űrlapok/js/main.js
+++ HTML űrlapok/js/main.js
@@ -1,1 +1,8 @@
-console.log("Console messages");
+function calcAmount(){
+    let price = 1200;
+    let amountInput = document.querySelector("input[name= 'amount-input']");
+    let amount = parseInt(amountInput.value) * price;
+    let showAmount = document.querySelector("span.show-amount");
+    
+    showAmount.innerHTML = amount;
+}
