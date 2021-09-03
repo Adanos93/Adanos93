709b29be15e2309f8edd98b48b26c46350e11ab1
Herczog Ádám <herczog.adam@Stiefel.local>
2021-09-03 14:55:36 +0200

    gyakorlás

--- HTML űrlapok/js/main.js
+++ HTML űrlapok/js/main.js
@@ -1,8 +1,24 @@
 function calcAmount(){
-    let price = 1200;
+    let price = 1000;
     let amountInput = document.querySelector("input[name= 'amount-input']");
-    let amount = parseInt(amountInput.value) * price;
-    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
     
+    showSumPrice(price, amountNumber);
+}
+
+function showSumPrice(price = 1000, amountNumber = 1){
+    let showAmount = document.querySelector("span.show-amount");
+    if (amountNumber > 10 || amountNumber < 1)
+    {
+        alert("Baaaakaaaaaa");
+        return;
+    }
+    
+    let amount = amountNumber * price;
+    let minorder = 5000;
+    if (amount < minorder){
+        amount = amount + 500;
+    }
     showAmount.innerHTML = amount;
 }
